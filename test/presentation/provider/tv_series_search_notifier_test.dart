b8324fb856374a41f48687485159b0fae66cd283
raka <rakafajar18@gmail.com>
2022-08-31 11:11:00 +0700

    unit testing provider

---
+++ test/presentation/provider/tv_series_search_notifier_test.dart
@@ -0,0 +1,84 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/tv_series/search_tv_series.dart';
+import 'package:ditonton/presentation/provider/tv_series_search_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'tv_series_search_notifier_test.mocks.dart';
+
+@GenerateMocks([SearchTvSeries])
+void main() {
+  late TvSeriesSearchNotifier provider;
+  late MockSearchTvSeries mockSearchTvSeries;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockSearchTvSeries = MockSearchTvSeries();
+    provider = TvSeriesSearchNotifier(searchTvSeries: mockSearchTvSeries)
+      ..addListener(() {
+        listenerCallCount += 1;
+      });
+  });
+
+  final tTvSeriesModel = TvSeries(
+    backdropPath: "/ypLoTftyF5EpGBxJas4PThIdiU4.jpg",
+    firstAirDate: "2010-06-08",
+    genreIds: [18, 9648],
+    id: 31917,
+    name: "Pretty Little Liars",
+    originCountry: ["US"],
+    originalLanguage: "en",
+    originalName: "Pretty Little Liars",
+    overview:
+        "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.",
+    popularity: 239.476,
+    posterPath: "/aUPbHiLS3hCHKjtLsncFa9g0viV.jpg",
+    voteAverage: 8.033,
+    voteCount: 2190,
+  );
+
+  final tTvSeriesList = <TvSeries>[tTvSeriesModel];
+  final tQuery = 'Pretty Little Liars';
+
+  group('search tv series', () {
+    test('should change state to loading when usecase is called', () async {
+      // arrange
+      when(mockSearchTvSeries.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvSeriesList));
+      // act
+      provider.fetchTvSeriesSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loading);
+    });
+
+    test('should change search result data when data is gotten successfully',
+        () async {
+      // arrange
+      when(mockSearchTvSeries.execute(tQuery))
+          .thenAnswer((_) async => Right(tTvSeriesList));
+      // act
+      await provider.fetchTvSeriesSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Loaded);
+      expect(provider.searchResult, tTvSeriesList);
+      expect(listenerCallCount, 2);
+    });
+
+    test('should return error when data is unsuccessful', () async {
+      // arrange
+      when(mockSearchTvSeries.execute(tQuery))
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      // act
+      await provider.fetchTvSeriesSearch(tQuery);
+      // assert
+      expect(provider.state, RequestState.Error);
+      expect(provider.message, 'Server Failure');
+      expect(listenerCallCount, 2);
+    });
+  });
+}
---
+++ test/presentation/provider/tv_series_watchlist_notifier_test.dart
@@ -0,0 +1,58 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/usecases/tv_series/get_tv_series_watchlist.dart';
+import 'package:ditonton/presentation/provider/tv_series_watchlist_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../dummy_data/dummy_objects.dart';
+import 'tv_series_watchlist_notifier_test.mocks.dart';
+
+@GenerateMocks([GetTvSeriesWatchlist])
+void main() {
+  late TvSeriesWatchlistNotifier provider;
+  late MockGetTvSeriesWatchlist mockGetTvSeriesWatchlist;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetTvSeriesWatchlist = MockGetTvSeriesWatchlist();
+    provider = TvSeriesWatchlistNotifier(
+      getTvSeriesWatchlist: mockGetTvSeriesWatchlist,
+    )..addListener(() {
+        listenerCallCount += 1;
+      });
+  });
+
+  test('should change tv series data when data is gotten successfully',
+      () async {
+    // arrange
+    when(mockGetTvSeriesWatchlist.execute())
+        .thenAnswer((_) async => Right([testTvSeriesListWatchlist]));
+    // act
+    await provider.fetchTvSeriesWatchlistMovies();
+    // assert
+    expect(provider.tvSeriesState, RequestState.Loaded);
+    expect(provider.tvSeriesWatchlist, [testTvSeriesListWatchlist]);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when data is unsuccessful', () async {
+    // arrange
+    when(mockGetTvSeriesWatchlist.execute()).thenAnswer(
+      (_) async => Left(
+        DatabaseFailure(
+          "Can't get data",
+        ),
+      ),
+    );
+    // act
+    await provider.fetchTvSeriesWatchlistMovies();
+    // assert
+    expect(provider.tvSeriesState, RequestState.Error);
+    expect(provider.message, "Can't get data");
+    expect(listenerCallCount, 2);
+  });
+}
