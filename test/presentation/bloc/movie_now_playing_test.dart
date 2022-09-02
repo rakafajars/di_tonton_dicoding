3d08f69cc738a61cddc53f5f9b743694e53063fe
raka <rakafajar18@gmail.com>
2022-09-03 02:10:18 +0700

    unit testing movie now playing

---
+++ test/presentation/bloc/movie_now_playing_test.dart
@@ -0,0 +1,91 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_now_playing_movies.dart';
+import 'package:ditonton/presentation/bloc/movie_now_playing/movie_now_playing_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../provider/movie_list_notifier_test.mocks.dart';
+
+@GenerateMocks([GetNowPlayingMovies])
+void main() {
+  late MovieNowPlayingBloc movieNowPlayingBloc;
+  late MockGetNowPlayingMovies mockGetNowPlayingMovies;
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovieList = <Movie>[tMovie];
+
+  setUp(() {
+    mockGetNowPlayingMovies = MockGetNowPlayingMovies();
+    movieNowPlayingBloc = MovieNowPlayingBloc(mockGetNowPlayingMovies);
+  });
+
+  // pengujian verivikasi awal
+  test('initial state should be empty', () {
+    expect(movieNowPlayingBloc.state, MovieNowPlayingEmpty());
+  });
+
+  blocTest(
+    'Should emit [Loading, HasData] when data is gotten successfully',
+    build: () {
+      when(mockGetNowPlayingMovies.execute()).thenAnswer(
+        (_) async => Right(
+          tMovieList,
+        ),
+      );
+
+      return movieNowPlayingBloc;
+    },
+    act: (MovieNowPlayingBloc bloc) => bloc.add(FetchNowPlayingMovieEvent()),
+    wait: const Duration(milliseconds: 500),
+    expect: () => [
+      MovieNowPlayingLoading(),
+      MovieNowPlayingHasData(tMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockGetNowPlayingMovies.execute());
+    },
+  );
+
+  blocTest(
+    'Should emit [Loading, Error] when get search is unsuccessful',
+    build: () {
+      when(mockGetNowPlayingMovies.execute()).thenAnswer(
+        (_) async => Left(
+          ServerFailure(
+            'Server Failure',
+          ),
+        ),
+      );
+
+      return movieNowPlayingBloc;
+    },
+    wait: const Duration(milliseconds: 500),
+    act: (MovieNowPlayingBloc bloc) => bloc.add(FetchNowPlayingMovieEvent()),
+    expect: () => [
+      MovieNowPlayingLoading(),
+      MovieNowPlayingError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockGetNowPlayingMovies.execute());
+    },
+  );
+}
