51950fd088b36d02a341913396c7c15f6df6d7bc
Raka Fajar <rakafajar18@gmail.com>
2022-09-03 17:55:44 +0700

    unit testing top rated & popular movie

---
+++ test/presentation/bloc/movie_popular_bloc_test.dart
@@ -0,0 +1,91 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_popular_movies.dart';
+import 'package:ditonton/presentation/bloc/movie_popular/movie_popular_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../provider/movie_list_notifier_test.mocks.dart';
+
+@GenerateMocks([GetPopularMovies])
+void main() {
+  late MoviePopularBloc moviePopularBloc;
+  late MockGetPopularMovies mockGetPopularMovies;
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovieList = <Movie>[tMovie];
+
+  setUp(() {
+    mockGetPopularMovies = MockGetPopularMovies();
+    moviePopularBloc = MoviePopularBloc(mockGetPopularMovies);
+  });
+
+  // pengujian verivikasi awal
+  test('initial state should be empty', () {
+    expect(moviePopularBloc.state, MoviePopularEmpty());
+  });
+
+  blocTest(
+    'Should emit [Loading, HasData] when data is gotten successfully',
+    build: () {
+      when(mockGetPopularMovies.execute()).thenAnswer(
+            (_) async => Right(
+          tMovieList,
+        ),
+      );
+
+      return moviePopularBloc;
+    },
+    act: (MoviePopularBloc bloc) => bloc.add(FetchMoviePopularEvent()),
+    wait: const Duration(milliseconds: 500),
+    expect: () => [
+      MoviePopularLoading(),
+      MoviePopularHasData(tMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockGetPopularMovies.execute());
+    },
+  );
+
+  blocTest(
+    'Should emit [Loading, Error] when get search is unsuccessful',
+    build: () {
+      when(mockGetPopularMovies.execute()).thenAnswer(
+            (_) async => Left(
+          ServerFailure(
+            'Server Failure',
+          ),
+        ),
+      );
+
+      return moviePopularBloc;
+    },
+    wait: const Duration(milliseconds: 500),
+    act: (MoviePopularBloc bloc) => bloc.add(FetchMoviePopularEvent()),
+    expect: () => [
+      MoviePopularLoading(),
+      MoviePopularError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockGetPopularMovies.execute());
+    },
+  );
+}
---
+++ test/presentation/bloc/movie_top_rated_bloc_test.dart
@@ -0,0 +1,92 @@
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_top_rated_movies.dart';
+import 'package:ditonton/presentation/bloc/movie_top_rated/movie_top_rated_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../provider/movie_list_notifier_test.mocks.dart';
+
+@GenerateMocks([GetTopRatedMovies])
+void main() {
+  late MovieTopRatedBloc movieTopRatedBloc;
+  late MockGetTopRatedMovies mockGetTopRatedMovies;
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: 'backdropPath',
+    genreIds: [1, 2, 3],
+    id: 1,
+    originalTitle: 'originalTitle',
+    overview: 'overview',
+    popularity: 1,
+    posterPath: 'posterPath',
+    releaseDate: 'releaseDate',
+    title: 'title',
+    video: false,
+    voteAverage: 1,
+    voteCount: 1,
+  );
+
+  final tMovieList = <Movie>[tMovie];
+
+  setUp(() {
+    mockGetTopRatedMovies = MockGetTopRatedMovies();
+    movieTopRatedBloc = MovieTopRatedBloc(mockGetTopRatedMovies);
+  });
+
+// pengujian verivikasi awal
+  test('initial state should be empty', () {
+    expect(movieTopRatedBloc.state, MovieTopRatedEmpty());
+  });
+
+  blocTest(
+    'Should emit [Loading, HasData] when data is gotten successfully',
+    build: () {
+      when(mockGetTopRatedMovies.execute()).thenAnswer(
+            (_) async => Right(
+          tMovieList,
+        ),
+      );
+
+      return movieTopRatedBloc;
+    },
+    act: (MovieTopRatedBloc bloc) => bloc.add(FetchMovieTopRatedEvent()),
+    wait: const Duration(milliseconds: 500),
+    expect: () => [
+      MovieTopRatedLoading(),
+      MovieTopRatedHasData(tMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedMovies.execute());
+    },
+  );
+
+  blocTest(
+    'Should emit [Loading, Error] when get search is unsuccessful',
+    build: () {
+      when(mockGetTopRatedMovies.execute()).thenAnswer(
+            (_) async => Left(
+          ServerFailure(
+            'Server Failure',
+          ),
+        ),
+      );
+
+      return movieTopRatedBloc;
+    },
+    wait: const Duration(milliseconds: 500),
+    act: (MovieTopRatedBloc bloc) => bloc.add(FetchMovieTopRatedEvent()),
+    expect: () => [
+      MovieTopRatedLoading(),
+      MovieTopRatedError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedMovies.execute());
+    },
+  );
+}
